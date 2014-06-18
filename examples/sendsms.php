a8ee200505558491b27c9bd84d56e7cea458264f
Rasheeda Yehuza <rashkeed@gmail.com>
2014-06-18 17:01:05 +0000

    enhancements made to sending messages example code.

--- examples/sendsms.php
+++ examples/sendsms.php
@@ -4,11 +4,28 @@ require_once('../nasaramobileapiclient.php');
 
 $phone = "233244209193";
 $senderId = "NTesting";
-$message = "hey there, this is a test messge!";
+$message = "hey there, this is a test message!";
 
 $smsApi = new NasaraMobileApiClient;
 $result = $smsApi->sendSms($phone, $senderId, $message);
 
-print($result);
+if($result == '1801'){
+    echo "message sent successfully";
+}
+elseif($result == '1802') {
+    echo "message sending failed";
+}
+elseif($result == '1803') {
+    echo "invalid login phone or password given";
+}
+elseif($result == '1804') {
+    echo "not enough sms credit";
+}
+elseif($result == '1805') {
+    echo "sender id must be more than 1 charater and less than 12 characters";
+}
+elseif($result == '1806') {
+    echo "phone number must be more than 8 characters";
+}
 
 
